b284aedf6a97e6c8d6715eb1291f222a460bece5
bysalkarki <56023708+bysalkarki@users.noreply.github.com>
2026-08-22 16:36:02 +0545

    Add React app source files (main.tsx, App.tsx, App.css, index.css)

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Reads from "./pages/Reads";
-import NotFound from "./pages/NotFound";
+import './App.css'
 
-const queryClient = new QueryClient();
+function App() {
+  return (
+    <div className="App">
+      <h1>Bishal Karki</h1>
+      <p>PHP & Node.js Backend Developer</p>
+      <p>Portfolio Coming Soon</p>
+    </div>
+  )
+}
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* Unlisted, URL-only reading log */}
-          <Route path="/reads" element={<Reads />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default App
